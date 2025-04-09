a3b26f6865c8d3e6bb53085d0ccfb429cd683ec0
Maxime-T <maxime.tscharner@gmail.com>
2025-04-10 01:15:12 +0200

    parser update

--- src/ast/parser.rs
+++ src/ast/parser.rs
@@ -20,9 +20,14 @@ Token: logos::Logos<'source> + Debug, {
 }
 
 fn parse_line(line : &[Token]) -> LineEnum {
-    match line[0] {
+    match &line[0] {
         Token::RETURN => LineEnum::Statement( StatementEnum::Return(Box::new(parse_expr(&line[1..]))) ),
-        Token::LET => todo!(),
+        Token::LET => {
+            match &line[1] {
+                Token::VARIABLE(s) => LineEnum::Statement(StatementEnum::Let(s.clone(), Box::new(parse_expr(&line[2..])))), //no equals sign for now
+                _ => panic!("Expected variable name")
+            }
+        },
         Token::PRINT => todo!(),
         Token::OPEN => todo!(),
         Token::CLOSE => todo!(),
@@ -34,8 +39,20 @@ fn parse_line(line : &[Token]) -> LineEnum {
 
 fn parse_expr(line : &[Token]) -> ExpressionEnum {
     match &line[0] {
-        Token::NUMBER(_) => todo!(),
-        Token::VARIABLE(_) => todo!(),
+        Token::NUMBER(_) => {
+            let op = line.iter().find(|t| matches!(t, Token::OPERATOR(_)));
+            match op {
+                Some(_) => ExpressionEnum::Operation(parse_opp(line)),
+                None => ExpressionEnum::Value(parse_value(line)),
+            }
+        },
+        Token::VARIABLE(_) => {
+            let op = line.iter().find(|t| matches!(t, Token::OPERATOR(_)));
+            match op {
+                Some(_) => ExpressionEnum::Operation(parse_opp(line)),
+                None => ExpressionEnum::Value(parse_value(line)),
+            }
+        },
         Token::OPEN => {
             let end = line.iter().enumerate().find(|(i, t)| **t == Token::CLOSE).expect("missing closing brackets").0;
             let inside = &line[1..end];
@@ -46,6 +63,7 @@ fn parse_expr(line : &[Token]) -> ExpressionEnum {
             }
         },
 
+        Token::CLOSE => panic!("no matching openning parenthesis"),
         _ => panic!("Unexpected token, expected expression"),
     }
 }
